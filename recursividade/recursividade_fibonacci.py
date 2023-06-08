46cb54fca43f159e4464ddc91614ccaee1d611ba
PedroCarpe <dd2426016@gmail.com>
2023-06-08 12:00:13 -0300

    fibonacci

--- recursividade/recursividade_fibonacci.py
+++ recursividade/recursividade_fibonacci.py
@@ -1,16 +1,16 @@
 'https://www.mathsisfun.com/numbers/fibonacci-sequence.html'
 
-def fibonacci(num,l):
+def fibonacci(num,l,comprimento):
     lista = l
     lista.append(num)
     
     if len(lista) == 1:
         lista.append(num)
 
-    if len(lista) == 11:
+    if len(lista) == comprimento:
         return lista
     else:
-        return fibonacci(lista[-1] + lista[-2],lista)           
+        return fibonacci(lista[-1] + lista[-2],lista,comprimento)           
 
-print(fibonacci(1,[]))
+print(fibonacci(1,[],11))
         
